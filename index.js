fc4ce69e9fd25ce6ab9fe3de0fbe3cc20025e443
Jose Gomez <pepguzman300@gmail.com>
2025-07-31 07:42:25 -0500

    Final change

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let temp = prompt("Enter a Temperature")
+
+
+function converToCelcius(temp){
+return (temp - 32) * 5/9
+}
+ celcius = converToCelcius(temp)
+ description = describeTemperature(temp);
+
+function describeTemperature(temp){
+if (temp <= 0)
+    return `VERY COLD!`
+else if (temp < 20)
+    return `COLD!`
+else if(temp < 30)
+    return `WARM!`
+else if (temp < 40)
+    return `HOT!`
+else;
+return `VERY HOT!`
+
+}
+
+console.log("The temperature in F to C is ", celcius, "And its " , description )
+
+
+
